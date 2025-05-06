767e655ebdac90b123fe02c5396555a96e31fa35
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-05-06 23:17:30 +0530

    Completing Project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,6 +24,7 @@ const Footer = () => {
             </p>
           </div>
 
+          {/* This is links section */}
           {/* Links */}
           <div className="grid grid-cols-2 md:grid-cols-3 gap-x-12 gap-y-2 text-sm">
             <Link
